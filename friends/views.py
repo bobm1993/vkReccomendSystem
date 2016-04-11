b900b1d0b29b973895c4f4e5db0d7b7e30d2f044
bobm1993 <bob.m.1993@gmail.com>
2016-04-11 23:03:03 +0300

    friends top

--- friends/views.py
+++ friends/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 import vk
+import vk.exceptions
 import time
+import socket
+import errno
+
+host = '127.0.0.1'
+port = 8000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def home(request):
@@ -9,22 +16,77 @@ def home(request):
 
 
 def friends_list(request):
+    current_id = request.POST.get('your_id')
+    friends = friends_get(current_id)
+    super_friend_list = []
+    full_super_friend_list = []
+    rates = []
+    k = 0
+    for friend in friends:
+        time.sleep(0.34)
+        fr_friends = friends_get(friend)
+        for fr in fr_friends:
+            if (fr not in friends) and (fr not in super_friend_list):
+                super_friend_list.append(fr)
+            if fr not in friends:
+                full_super_friend_list.append(fr)
+
+    for idx, sup in enumerate(super_friend_list):
+        i = 0
+        for full_sup in full_super_friend_list:
+            if sup == full_sup:
+                i += 1
+        rates[idx] = i
+        k += rates[idx]
+    for idx, rat in enumerate(rates):
+        rates[idx] = rat / k
+    diction = dict(zip(super_friend_list, rates))
+    diction_sort = sorted(diction.items(), reverse=True)
+    diction_top10 = diction_sort[:10]
+
+    # time.sleep(0.34)
+    # cities = []
+    # for friend in friends:
+    #     try:
+    #         time.sleep(0.34)
+    #         x = vkapi.users.get(user_id=friend, fields=['city'])
+    #         city_id = x[0]['city']
+    #         time.sleep(0.34)
+    #         k = vkapi.database.getCitiesById(city_ids=city_id)
+    #         if k:
+    #             cities.append(k[0]['name'])
+    #         time.sleep(0.34)
+    #     except KeyError:
+    #         pass
+    return render(request, 'friends/recommend_list.html', {'rec_list': diction_top10})
+
+
+def friends_get(ide):
     session = \
         vk.Session(access_token='be2ed15f660f93722c8be1255d7e65a47c2350a220231aa90514029c0c2b2ff071548c66ed56edd275fd2')
     vkapi = vk.API(session)
-    current_id = request.POST.get('your_id')
-    friends = vkapi.friends.get(user_id=current_id)
-    time.sleep(0.34)
-    cities = []
-    for friend in friends:
-        try:
-            x = vkapi.users.get(user_id=friend, fields=['city'])
-            city_id = x[0]['city']
-            time.sleep(0.34)
-            k = vkapi.database.getCitiesById(city_ids=city_id)
-            if k:
-                cities.append(k[0]['name'])
-            time.sleep(0.34)
-        except KeyError:
-            pass
-    return render(request, 'friends/recommend_list.html', {'cities': cities})
+    friends_me = []
+    try:
+        friends_me = vkapi.friends.get(user_id=ide)
+    except vk.exceptions.VkException:
+        pass
+    except socket.error as error:
+        if error.errno == errno.WSAECONNRESET:
+            connect()
+        else:
+            raise
+    return friends_me
+
+
+def loop():
+    try:
+        while 1:
+            print(s.recv(512))
+    except socket.error:
+        s.close()
+        connect()
+
+
+def connect():
+    s.connect((host, port))
+    loop()
