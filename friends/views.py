652863f5adbd02bf7f9129ea1365cccc3fc846d4
bobm1993 <bob.m.1993@gmail.com>
2016-04-12 22:39:03 +0300

    fixed

--- friends/views.py
+++ friends/views.py
@@ -21,6 +21,7 @@ def friends_list(request):
     friends = friends_get(current_id)
     full_super_friend_list = []
     rates = []
+    recommends_top10 = []
     for friend in friends:
         time.sleep(0.1)
         fr_friends = friends_get(friend)
@@ -33,29 +34,19 @@ def friends_list(request):
 
     k = len(full_super_friend_list)
     for sup in super_friend_list:
-        i = full_super_friend_list.count(sup)/k
+        i = full_super_friend_list.count(sup) / k
         print(i)
         rates.append(i)
 
+    # recommends = users_get(super_friend_list)
     diction = dict(zip(super_friend_list, rates))
     diction_sort = sorted(diction.items(), key=operator.itemgetter(1), reverse=True)
     diction_top10 = diction_sort[:10]
 
-    # time.sleep(0.34)
-    # cities = []
-    # for friend in friends:
-    #     try:
-    #         time.sleep(0.34)
-    #         x = vkapi.users.get(user_id=friend, fields=['city'])
-    #         city_id = x[0]['city']
-    #         time.sleep(0.34)
-    #         k = vkapi.database.getCitiesById(city_ids=city_id)
-    #         if k:
-    #             cities.append(k[0]['name'])
-    #         time.sleep(0.34)
-    #     except KeyError:
-    #         pass
-    return render(request, 'friends/recommend_list.html', {'rec_list': diction_top10})
+    for row in diction_top10:
+        recommends_top10.append(row[0])
+
+    return render(request, 'friends/recommend_list.html', {'rec_list': recommends_top10})
 
 
 def friends_get(ide):
@@ -73,6 +64,22 @@ def friends_get(ide):
         else:
             raise
     return friends_me
+
+
+# def users_get(profiles):
+#     session = \
+#         vk.Session(access_token='be2ed15f660f93722c8be1255d7e65a47c2350a220231aa90514029c0c2b2ff071548c66ed56edd275fd2')
+#     vkapi = vk.API(session)
+#     names = []
+#     for profile in profiles:
+#         try:
+#             x = vkapi.users.get(user_id=profile)
+#             names.append(x[0])
+#             print(x)
+#         except KeyError:
+#             pass
+#         time.sleep(0.34)
+#     return names
 
 
 def loop():
